3797dcf451c85db6650fb54d36463d4410c1d2ac
davarezza <dapva2728@gmail.com>
2024-04-08 08:58:01 +0700

    feat: complete the landing page view and separate each component for better development

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,88 @@
+<div class="kategory-foot3">
+    <p class="fw-bold text-muted">Nikmati Mudahnya Menjual Barang Bekas di ThriftHaven</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">ThriftHaven adalah sebuah aplikasi yang dirancang untuk memudahkan pengguna dalam <span class="fw-bold text-muted">menjual barang bekas</span> mereka dengan cepat dan efisien. Dengan platform ThriftHaven, pengguna dapat dengan mudah memperoleh manfaat dari barang-barang yang tidak lagi mereka butuhkan dengan cara menjualnya kepada orang lain yang mungkin membutuhkannya. Aplikasi ini memberikan pengalaman yang <span class="fw-bold text-muted">nyaman dan efisien</span> bagi penjual barang bekas dengan berbagai fitur yang ditawarkan.</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">Salah satu fitur utamanya adalah kemampuan untuk mengunggah foto barang dengan cepat dan mudah, serta menambahkan deskripsi singkat untuk menjelaskan kondisi barang. Ini membantu menarik perhatian pembeli potensial dengan cara visual. Selain itu, tersedia berbagai pilihan harga dan sistem negosiasi yang memungkinkan penjual dan pembeli mencapai kesepakatan yang menguntungkan kedua belah pihak. Dengan fitur ini, penjual dapat menyesuaikan harga barang sesuai dengan nilai dan kondisi barang yang mereka tawarkan. ThriftHaven juga terintegrasi dengan sistem beberapa jasa ekspedisi untuk memudahkan proses pengiriman barang. Pengguna dapat memilih jasa ekspedisi yang mereka inginkan dan menyesuaikan opsi pengiriman sesuai dengan preferensi mereka. Dengan integrasi ini, proses pengiriman barang menjadi lebih mudah dan efisien bagi penjual dan pembeli.</p>
+        
+    <p class="fw-bold text-muted">Belanja Produk-produk Bekas Terbaik di ThriftHaven</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">ThriftHaven adalah platform inovatif yang menyediakan pengalaman berbelanja produk-produk bekas <span class="fw-bold text-muted">terbaik</span> secara online. Dibangun dengan tujuan untuk menyediakan akses mudah dan terjangkau terhadap barang-barang berkualitas tinggi yang telah digunakan sebelumnya, ThriftHaven memberikan solusi yang memuaskan bagi para pencinta belanja yang mencari barang-barang unik dan berkualitas tanpa harus mengeluarkan biaya besar. ThriftHaven menawarkan beragam koleksi barang bekas yang dapat dipilih oleh pengguna. Dari pakaian, peralatan elektronik, perlengkapan rumah tangga, hingga barang-barang koleksi dan antik, pengguna dapat menemukan berbagai produk yang menarik dan unik di platform ini. Setiap barang yang ditawarkan telah melewati proses seleksi yang ketat untuk memastikan kualitas dan keaslian produk, sehingga pengguna dapat berbelanja dengan percaya diri dan yakin akan kualitas barang yang mereka dapatkan.</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">Salah satu keunggulan utama dari ThriftHaven adalah <span class="fw-bold text-secondary">harga terjangkau</span> yang ditawarkan untuk produk-produk bekas berkualitas tinggi. Dengan harga yang lebih rendah daripada barang-barang baru di pasaran, pengguna dapat menghemat uang sambil tetap mendapatkan barang-barang berkualitas yang mereka butuhkan. Selain itu, ThriftHaven juga seringkali menawarkan penawaran spesial dan diskon eksklusif kepada para pelanggan, sehingga mereka dapat mendapatkan nilai tambah dari setiap pembelian yang mereka lakukan. ThriftHaven menempatkan <span class="fw-bold text-muted">keamanan dan kenyamanan</span> pengguna sebagai prioritas utama. Dengan antarmuka pengguna yang ramah dan intuitif, pengguna dapat dengan mudah menjelajahi koleksi barang, menemukan produk yang mereka cari, dan melakukan pembelian dengan cepat dan aman. Selain itu, ThriftHaven juga menyediakan berbagai pilihan metode pembayaran yang aman dan terpercaya, serta menjamin keamanan data pribadi pengguna selama proses transaksi.</p>
+
+    <p class="fw-bold text-muted">Mendukung Lingkungan dan Ekonomi Berkelanjutan</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">ThriftHaven tidak hanya sebuah platform belanja online biasa, tetapi juga sebuah inisiatif yang berkomitmen untuk mendukung lingkungan dan ekonomi yang berkelanjutan. Dengan menyediakan akses mudah terhadap barang-barang bekas berkualitas tinggi, ThriftHaven berperan dalam mempromosikan praktik berbelanja yang bertanggung jawab dan ramah lingkungan. Salah satu cara utama ThriftHaven mendukung lingkungan adalah melalui <span class="fw-bold text-muted">pengurangan limbah</span> dan pemanfaatan kembali barang-barang bekas. Dengan memberikan platform untuk penjualan barang bekas, ThriftHaven membantu mengurangi jumlah barang yang dibuang ke tempat pembuangan sampah. Sebagai hasilnya, limbah yang dihasilkan dari barang-barang tersebut dapat diminimalkan, membantu mengurangi dampak negatif terhadap lingkungan. ThriftHaven juga berperan dalam mempromosikan praktik berbelanja yang berkelanjutan dengan mengedukasi masyarakat tentang pentingnya membeli barang bekas dan <span class="fw-bold text-muted">mengurangi konsumsi berlebihan</span>. Dengan menawarkan alternatif yang terjangkau dan berkualitas tinggi untuk barang-barang baru, ThriftHaven mendorong masyarakat untuk lebih memilih barang bekas sebagai opsi belanja yang ramah lingkungan.</p>
+    <p class="fw-300 text-secondary" style="font-size: .9rem">Selain mendukung lingkungan, ThriftHaven juga memberikan kontribusi positif terhadap ekonomi lokal dengan mendukung penjual independen dan pelaku usaha kecil. Dengan memberikan platform untuk memasarkan barang-barang mereka kepada masyarakat luas, ThriftHaven membantu meningkatkan pendapatan dan memperluas jangkauan pasar bagi para penjual lokal. Hal ini tidak hanya menguntungkan para penjual, tetapi juga memperkuat ekonomi lokal secara keseluruhan. ThriftHaven juga menjalin kemitraan dengan organisasi lingkungan dan amal untuk mendukung misi perlindungan lingkungan yang lebih luas. Melalui kerjasama dengan organisasi-organisasi tersebut, ThriftHaven menggalang dukungan dan meningkatkan kesadaran tentang isu-isu lingkungan yang penting, serta memberikan kontribusi finansial untuk mendukung proyek-proyek konservasi dan keberlanjutan.</p>
+
+
+        
+    <div class="row mx-0 mt-4">
+        <div class="col-4">
+            <div class="d-flex align-items-center">
+                <img class="mr-3" src="assets/img/18.png" alt="">
+                <div class="d-flex flex-column">
+                    <span class="mb-3 fw-bold" style="color: #03ac0e;">Transparent</span>
+                    <p>Pembayaran Anda baru saja diteruskan kepenjual setelah barang diterima</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="d-flex align-items-center">
+                <img class="mr-3" src="assets/img/18.png" alt="">
+                <div class="d-flex flex-column">
+                    <span class="mb-3 fw-bold" style="color: #03ac0e;">Transparent</span>
+                    <p>Pembayaran Anda baru saja diteruskan kepenjual setelah barang diterima</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="d-flex align-items-center">
+                <img class="mr-3" src="assets/img/18.png" alt="">
+                <div class="d-flex flex-column">
+                    <span class="mb-3 fw-bold" style="color: #03ac0e;">Transparent</span>
+                    <p>Pembayaran Anda baru saja diteruskan kepenjual setelah barang diterima</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mx-0 mt-4 footer" style="padding: 50px;">
+        <div class="col-3">
+            <div class="d-flex flex-column">
+                <h4 class="fw-bold text-muted mb-3">Tokopedia</h4>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="d-flex flex-column">
+                <h4 class="fw-bold text-muted mb-3">Tokopedia</h4>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="d-flex flex-column">
+                <h4 class="fw-bold text-muted mb-3">Tokopedia</h4>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+                <p>Tentang tokopedia</p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="d-flex flex-column align-items-center">
+                <img src="assets/img/21.png" width="400" alt="">
+                <div class="d-flex mt-3">
+                    <img width="150" class="mr-3" src="assets/img/22.png" alt="">
+                    <img width="150" src="assets/img/23.png" alt="">
+                </div>
+                <span>2019 - 2020 Tokopedia</span>
+                <div class="d-flex align-items-center mt-3">
+                    <span class="text-white fw-bold p-2 mr-3 rounded"
+                        style="background-color: #03ac0e;">Indonesia</span>
+                    <span>English</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/secondcard.blade.php
@@ -0,0 +1,283 @@
+<div class="d-flex" data-spy="affix" data-offset-top="1200">
+    <div class="position-relative mx-3 box-affix bg-affix1">
+        <div class="line1"></div>
+        <h4>For Your</h4>
+    </div>
+    <div class="position-relative mx-3 box-affix bg-affix2">
+        <div class="line1"></div>
+        <h4>Spesial Diskon</h4>
+    </div>
+</div>
+
+<div class="row mx-0 mt-4">
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mx-0 mt-2">
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-2 pl-0">
+        <div class="card">
+            <img src="assets/img/12.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">Kaos Cowo trending</h6>
+                <div class="d-flex">
+                    <span class="badge badge-pill"
+                        style="background-color: #ff00d13b; color: d50000;">47%</span>
+                    <span class="badge-pill badge" style="color: #989898;"><del>Rp 75.000</del></span>
+                </div>
+                <h6 class="mt-1">Rp 40.000</h6>
+                <div class="d-flex align-items-center">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <img class="mx-1" width="10" height="10" src="assets/img/icon-bintang.png" alt="">
+                    <span style="color: #989898;">(14)</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row mx-0 mt-4 mb-3 justify-content-center">
+    <button class="btn btn-green">Muat Lebih Banyak</button>
+</div>
